aeb72cee079d326edd94d6369f76ed5d40bba45a
m21a18 <m1m21a18@oit.ac.jp>
2021-07-16 22:58:39 +0900

    add gradle project

--- app/src/test/java/sequality/CalculateTest.java
+++ app/src/test/java/sequality/CalculateTest.java
@@ -33,45 +33,6 @@ public class CalculateTest {
   }
 
   @Test
-  public void testcodd() {
-    Calculate calculate = new Calculate();
-    boolean expected_true = true;
-    boolean expected_false = false;
-    for (int i = 1; i <= 10; i++) {
-      if (i == 1) {
-        assertEquals(expected_true, calculate.codd(1));
-      }
-      if (i == 2) {
-        assertEquals(expected_false, calculate.codd(2));
-      }
-      if (i == 3) {
-        assertEquals(expected_true, calculate.codd(3));
-      }
-      if (i == 4) {
-        assertEquals(expected_false, calculate.codd(4));
-      }
-      if (i == 5) {
-        assertEquals(expected_true, calculate.codd(5));
-      }
-      if (i == 6) {
-        assertEquals(expected_false, calculate.codd(6));
-      }
-      if (i == 7) {
-        assertEquals(expected_true, calculate.codd(7));
-      }
-      if (i == 8) {
-        assertEquals(expected_false, calculate.codd(8));
-      }
-      if (i == 9) {
-        assertEquals(expected_true, calculate.codd(9));
-      }
-      if (i == 10) {
-        assertEquals(expected_false, calculate.codd(10));
-      }
-    }
-  }
-
-  @Test
   public void testsumeven() {
     Calculate calculate = new Calculate();
     int expected = 30;
